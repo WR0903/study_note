b3171bbd4a79a137ee1266b24c0a6bff3c6b4766
WR <wangran@stu.hit.edu.cn>
2018-01-04 22:03:27 +0800

    rgb

---
+++ RGB-D/PART2/build-CODE-unknown-Default/src/CMakeFiles/slambase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slambase.dir/slamBase.cpp.o"
+  "/home/wr/WHR/RGB-D/PART2/CODE/lib/libslambase.pdb"
+  "/home/wr/WHR/RGB-D/PART2/CODE/lib/libslambase.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slambase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
