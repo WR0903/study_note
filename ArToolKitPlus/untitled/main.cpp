3b68eec93f4e39a5d51cc99254d5e96bd9e8d2f1
WR0903 <wangran@stu.hit.edu.cn>
2018-05-28 10:46:35 +0800

    maek

--- ArToolKitPlus/untitled/main.cpp
+++ ArToolKitPlus/untitled/main.cpp
@@ -1,24 +1,42 @@
 #include <cstdio>
 #include <ARToolKitPlus/TrackerSingleMarker.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <iostream>
 using ARToolKitPlus::TrackerSingleMarker;
 using namespace cv;
 using namespace std;
+std::string  int2str(int num);
+void drawMarkerInfo(cv::Mat &image,ARToolKitPlus::ARMarkerInfo* markInfo);
 
 int main(int argc, char** argv)
 {
     const bool useBCH = false;//选择哪一种二维码
     const int width = 640, height = 480, bpp = 1;
     size_t numPixels = width * height * bpp;
+    ARToolKitPlus::ARMarkerInfo* markInfo;
+    int markNum;
 
     unsigned char cameraBuffer[numPixels];
-    //cout<<cameraBuffer[1]<<endl;
     VideoCapture capture(0);
+    TrackerSingleMarker tracker(width, height, 8, 6, 6, 6, 0);
+    tracker.setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
+    if (!tracker.init("/home/wr/study_note/ArToolKitPlus/untitled/PGR_M12x0.5_2.5mm.cal", 1.0f, 1000.0f)) // load MATLAB file
+    {
+        printf("ERROR: init() failed\n");
+        return -1;
+    }
+    tracker.setPatternWidth(7);
+    tracker.setBorderWidth(useBCH ? 0.125 : 0.25);
+    tracker.setThreshold(150);//阈值，也可以自动阈值
+    tracker.setUndistortionMode(ARToolKitPlus::UNDIST_LUT);
+    tracker.setMarkerMode(useBCH ? ARToolKitPlus::MARKER_ID_BCH : ARToolKitPlus::MARKER_ID_SIMPLE);
     while(1)
     {
-        Mat frame;//=imread("/home/wr/soft/ARToolKitPlus/ARToolKitPlus-2.3.1/sample/data/image_320_240_8_marker_id_simple_nr031.jpg");
+        Mat frame,src;
         capture>>frame;
+        frame.copyTo(src);
         cv::cvtColor(frame,frame,cv::COLOR_BGR2GRAY);
         for(int ii=0;ii<frame.rows;ii++)
         {
@@ -28,55 +46,59 @@ int main(int argc, char** argv)
                 cameraBuffer[640*ii+jj]=data[jj];
             }
         }
-        imshow("1",frame);
-        TrackerSingleMarker tracker(width, height, 8, 6, 6, 6, 0);
-        tracker.setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
-        if (!tracker.init("/home/wr/study_note/ArToolKitPlus/untitled/PGR_M12x0.5_2.5mm.cal", 1.0f, 1000.0f)) // load MATLAB file
-        {
-            printf("ERROR: init() failed\n");
-            return -1;
-        }
 
-       // tracker.getCamera()->printSettings();
-
-        // define size of the marker in OpenGL units
-        tracker.setPatternWidth(7);
-
-        // the marker in the BCH test image has a thin border...
-        tracker.setBorderWidth(useBCH ? 0.125 : 0.25);
-
-        // set a threshold. alternatively we could also activate automatic thresholding
-        tracker.setThreshold(150);//阈值，也可以自动阈值
-
-        // let's use lookup-table undistortion for high-speed
-        // note: LUT only works with images up to 1024x1024
-        tracker.setUndistortionMode(ARToolKitPlus::UNDIST_LUT);
-
-        // switch to simple ID based markers
-        // use the tool in tools/IdPatGen to generate markers
-        tracker.setMarkerMode(useBCH ? ARToolKitPlus::MARKER_ID_BCH : ARToolKitPlus::MARKER_ID_SIMPLE);
-
-        // do the OpenGL camera setup
-        // glMatrixMode(GL_PROJECTION)
-        // glLoadMatrixf(tracker.getProjectionMatrix());
-
-        // here we go, just two calls to find the camera pose
-        std::vector<int> markerId = tracker.calc(cameraBuffer);
+        std::vector<int> markerId = tracker.calc(cameraBuffer,&markInfo,&markNum);
         tracker.selectBestMarkerByCf();
         float conf = tracker.getConfidence();
-
-        // use the result of calc() to setup the OpenGL transformation
-        // glMatrixMode(GL_MODELVIEW)
-        // glLoadMatrixf(tracker.getModelViewMatrix());
         if(!markerId.empty())
         {
             printf("\n\nFound marker %d  (confidence %d%%)\n\nPose-Matrix:\n  ", markerId[0], (int(conf * 100.0f)));
             for (int i = 0; i < 16; i++)
                 printf("%.2f  %s", tracker.getModelViewMatrix()[i], (i % 4 == 3) ? "\n  " : "");
         }
+        drawMarkerInfo(src,markInfo);
+        imshow("1",src);
         waitKey(30);
     }
 
 
     return 0;
+}
+std::string  int2str(int num)
+{
+    std::stringstream ss;
+    ss << num;
+    std::string text = ss.str();
+    return text;
+}
+void drawMarkerInfo(cv::Mat &image,ARToolKitPlus::ARMarkerInfo* markInfo)
+{
+    cv::Point center,corner0,corner1,corner2,corner3 ;
+    center=cv::Point(markInfo->pos[0],markInfo->pos[1]);
+    corner0=cv::Point(markInfo->vertex[(4-markInfo->dir+0)%4][0],markInfo->vertex[(4-markInfo->dir+0)%4][1]);
+    corner1=cv::Point(markInfo->vertex[(4-markInfo->dir+1)%4][0],markInfo->vertex[(4-markInfo->dir+1)%4][1]);
+    corner2=cv::Point(markInfo->vertex[(4-markInfo->dir+2)%4][0],markInfo->vertex[(4-markInfo->dir+2)%4][1]);
+    corner3=cv::Point(markInfo->vertex[(4-markInfo->dir+3)%4][0],markInfo->vertex[(4-markInfo->dir+3)%4][1]);
+
+    cv::line(image,corner0,corner1,CV_RGB(255,0,0),1,8);
+    cv::line(image,corner1,corner2,CV_RGB(255,0,0),1,8);
+    cv::line(image,corner2,corner3,CV_RGB(255,0,0),1,8);
+    cv::line(image,corner3,corner0,CV_RGB(255,0,0),1,8);
+    cv::rectangle(image,cv::Point(center.x-1, center.y-1),cv::Point(center.x+1, center.y+1),CV_RGB(0,255,0),1,8); //圈取图像中心点
+
+    //string dir_str = int2str(one_mark.dir);
+    std::string tx0 = "0";
+    std::string tx1 = "1";
+    std::string tx2 = "2";
+    std::string tx3 = "3";
+
+    cv::putText(image,tx0,corner0,CV_FONT_HERSHEY_COMPLEX,1,CV_RGB(0,0,255));
+    cv::putText(image,tx1,corner1,CV_FONT_HERSHEY_COMPLEX,1,CV_RGB(0,0,255));
+    cv::putText(image,tx2,corner2,CV_FONT_HERSHEY_COMPLEX,1,CV_RGB(0,0,255));
+    cv::putText(image,tx3,corner3,CV_FONT_HERSHEY_COMPLEX,1,CV_RGB(0,0,255));
+
+
+    std::string text ="Id:"+ int2str(markInfo->id);
+    cv::putText(image,text,cv::Point(center.x+80,center.y),CV_FONT_HERSHEY_COMPLEX,1,CV_RGB(0,0,255));
+
 }
